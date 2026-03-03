a3e8b1a70a2ee736aa177fa8326425262f219955
THESHMIL0 <loolk448@gmail.com>
2026-03-03 17:20:43 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// This lets the phone know our app has a background worker
-self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed 😸');
-});
-
-// This is required for the worker to be valid, even if it does nothing yet
-self.addEventListener('fetch', (event) => {
-    // Just lets network requests pass through normally
-});
