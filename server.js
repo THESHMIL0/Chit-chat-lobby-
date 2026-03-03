57822af9f887f7ec09081a052d5058714e9ba6e6
THESHMIL0 <loolk448@gmail.com>
2026-03-04 03:44:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,29 @@ const io = new Server(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const activeUsers = {};
+// 🌟 NEW: Create a list to hold recent messages
+const messageHistory = [];
+const MAX_HISTORY = 50; // We will only keep the last 50 messages so the server doesn't get overloaded
 
 io.on('connection', (socket) => {
+    
     socket.on('new user', (username) => {
         activeUsers[socket.id] = username;
         io.emit('user list', Object.values(activeUsers));
+        
+        // 🌟 NEW: When someone joins, send THEM (and only them) the message history
+        socket.emit('chat history', messageHistory);
     });
 
     socket.on('chat message', (data) => {
+        // 🌟 NEW: Save the new message into our history array
+        messageHistory.push(data);
+        
+        // If we have more than 50 messages, remove the oldest one
+        if (messageHistory.length > MAX_HISTORY) {
+            messageHistory.shift(); 
+        }
+        
         io.emit('chat message', data);
     });
 
