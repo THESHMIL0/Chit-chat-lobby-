8c33d6f96b92c119bf67bdb35edcd7a05e873ff0
THESHMIL0 <loolk448@gmail.com>
2026-03-04 03:37:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 🌟 NEW: Object to keep track of active users
 const activeUsers = {};
 
 io.on('connection', (socket) => {
-    // 🌟 NEW: When a user joins, save their name and broadcast the updated list
     socket.on('new user', (username) => {
         activeUsers[socket.id] = username;
         io.emit('user list', Object.values(activeUsers));
@@ -23,12 +21,10 @@ io.on('connection', (socket) => {
         io.emit('chat message', data);
     });
 
-    // 🌟 NEW: Broadcast when someone is typing
     socket.on('typing', (data) => {
         socket.broadcast.emit('typing', data);
     });
 
-    // 🌟 NEW: When someone leaves, remove them and update the list
     socket.on('disconnect', () => {
         delete activeUsers[socket.id];
         io.emit('user list', Object.values(activeUsers));
