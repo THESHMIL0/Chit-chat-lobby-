077ba9014e756147af926ff5c3d5202173b35a2c
THESHMIL0 <loolk448@gmail.com>
2026-03-03 17:40:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
-// Import the tools we need
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 
-// Setup the server
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Tell the server to serve our frontend files from the "public" folder (we will make this later)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Listen for users connecting to our app
-io.on('connection', (socket) => {
-    console.log('A user connected!');
+// 🌟 NEW: Object to keep track of active users
+const activeUsers = {};
 
-    // When this user sends a message, broadcast it to everyone else
+io.on('connection', (socket) => {
+    // 🌟 NEW: When a user joins, save their name and broadcast the updated list
+    socket.on('new user', (username) => {
+        activeUsers[socket.id] = username;
+        io.emit('user list', Object.values(activeUsers));
+    });
+
     socket.on('chat message', (data) => {
         io.emit('chat message', data);
     });
 
-    // When the user leaves
+    // 🌟 NEW: Broadcast when someone is typing
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+    });
+
+    // 🌟 NEW: When someone leaves, remove them and update the list
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        delete activeUsers[socket.id];
+        io.emit('user list', Object.values(activeUsers));
     });
 });
 
-// Start the server on a port assigned by Render, or 3000 if testing locally
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
